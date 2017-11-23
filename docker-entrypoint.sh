58d3adfd4bf7c5816fe0182b1c215aa5fa346660
André Bauer <monotek23@gmail.com>
2017-11-23 21:26:00 +0100

    fixed readme

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ "$1" = 'zammad' ]; then
 
   if [ -z "${ES_HOST}" ] || [ -z "${DB_HOST}" ] || [ -z "${DB_USER}" ]  || [ -z "${DB_PASS}" ]  || [ -z "${DB_HOST}" ] || [ -z "${DB_NAME}" ]; then
